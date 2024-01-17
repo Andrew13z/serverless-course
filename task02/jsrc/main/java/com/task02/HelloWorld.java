2dfe11992082ff5d5dcc10f91c02aa17b61c5afa
Andrii Krokhta <nowayoutta13@gmail.com>
2024-01-17 17:50:31 +0200

    revert to initial

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -2,40 +2,23 @@ package com.task02;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
-import com.syndicate.deployment.annotations.LambdaUrlConfig;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
-import com.syndicate.deployment.annotations.lambda.LambdaLayer;
-import com.syndicate.deployment.model.ArtifactExtension;
-import com.syndicate.deployment.model.DeploymentRuntime;
-import com.syndicate.deployment.model.lambda.url.AuthType;
-import com.syndicate.deployment.model.lambda.url.InvokeMode;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @LambdaHandler(lambdaName = "hello_world",
-		roleName = "hello_world-role",
-		isPublishVersion = true,
-		aliasName = "${lambdas_alias_name}"
+	roleName = "hello_world-role",
+	isPublishVersion = true,
+	aliasName = "${lambdas_alias_name}"
 )
-@LambdaLayer(
-		layerName = "sdk-layer",
-		runtime = DeploymentRuntime.JAVA8,
-		artifactExtension = ArtifactExtension.ZIP
-)
-@LambdaUrlConfig(
-		authType = AuthType.NONE,
-		invokeMode = InvokeMode.BUFFERED
-)
-public class HelloWorld implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
 
-	@Override
-	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent apiGatewayProxyRequestEvent,
-													  Context context) {
-		return new APIGatewayProxyResponseEvent()
-				.withStatusCode(200)
-				.withBody("{\n" +
-						  "    \"statusCode\": 200,\n" +
-						  "    \"message\": \"Hello from Lambda\"\n" +
-						  "}");
+	public Map<String, Object> handleRequest(Object request, Context context) {
+		System.out.println("Hello from lambda");
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		resultMap.put("statusCode", 200);
+		resultMap.put("message", "Hello from Lambda");
+		return resultMap;
 	}
 }
