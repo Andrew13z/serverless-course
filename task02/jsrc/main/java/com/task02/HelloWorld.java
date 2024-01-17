c55ce7f7c936d4df6be4005485793c50a2457c9f
Andrii Krokhta <nowayoutta13@gmail.com>
2024-01-17 18:12:03 +0200

    update handler

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -2,13 +2,12 @@ package com.task02;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.syndicate.deployment.annotations.LambdaUrlConfig;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.lambda.url.AuthType;
 import com.syndicate.deployment.model.lambda.url.InvokeMode;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @LambdaHandler(lambdaName = "hello_world",
 		roleName = "hello_world-role",
@@ -19,13 +18,21 @@ import java.util.Map;
 		authType = AuthType.NONE,
 		invokeMode = InvokeMode.BUFFERED
 )
-public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
+public class HelloWorld implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
-		System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("message", "Hello from Lambda");
-		return resultMap;
+	@Override
+	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request,
+													  Context context) {
+		if ("GET".equals(request.getHttpMethod()) && request.getPath().endsWith("/hello")) {
+
+			return new APIGatewayProxyResponseEvent()
+					.withStatusCode(200)
+					.withBody("{\n" +
+									  "    \"statusCode\": 200,\n" +
+									  "    \"message\": \"Hello from Lambda\"\n" +
+									  "}");
+		}
+
+		return new APIGatewayProxyResponseEvent().withStatusCode(400);
 	}
 }
