eb296886d26cd90d4a9e2d5f25420a827622c659
Andrii Krokhta <nowayoutta13@gmail.com>
2024-01-17 18:17:27 +0200

    fix error

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -11,7 +11,7 @@ import com.syndicate.deployment.model.lambda.url.InvokeMode;
 
 @LambdaHandler(lambdaName = "hello_world",
 		roleName = "hello_world-role",
-		isPublishVersion = true,
+		isPublishVersion = true
 )
 @LambdaUrlConfig(
 		authType = AuthType.NONE,
