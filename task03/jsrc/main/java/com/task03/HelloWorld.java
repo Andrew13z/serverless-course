5895c3f0a1cba7af08f64dfc9ed97a061d15d4b1
Andrii Krokhta <nowayoutta13@gmail.com>
2024-01-24 15:53:50 +0200

    task 03

---
+++ task03/jsrc/main/java/com/task03/HelloWorld.java
@@ -0,0 +1,37 @@
+package com.task02;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.syndicate.deployment.annotations.LambdaUrlConfig;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
+
+@LambdaHandler(lambdaName = "hello_world",
+		roleName = "hello_world-role",
+		isPublishVersion = true
+)
+@LambdaUrlConfig(
+		authType = AuthType.NONE,
+		invokeMode = InvokeMode.BUFFERED
+)
+public class HelloWorld implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+	@Override
+	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request,
+													  Context context) {
+		if ("GET".equals(request.getHttpMethod()) && request.getPath().endsWith("/hello")) {
+
+			return new APIGatewayProxyResponseEvent()
+					.withStatusCode(200)
+					.withBody("{\n" +
+									  "    \"statusCode\": 200,\n" +
+									  "    \"message\": \"Hello from Lambda\"\n" +
+									  "}");
+		}
+
+		return new APIGatewayProxyResponseEvent().withStatusCode(400);
+	}
+}
