bb045311ce178f6093a8084acf8b900ffa30784b
Andrii Krokhta <nowayoutta13@gmail.com>
2024-01-26 22:14:43 +0200

    format

--- task05/jsrc/main/java/com/task05/ApiHandler.java
+++ task05/jsrc/main/java/com/task05/ApiHandler.java
@@ -58,7 +58,7 @@ public class ApiHandler implements RequestHandler<ApiRequest, APIGatewayProxyRes
 		event.setId(UUID.randomUUID().toString());
 		event.setPrincipalId(apiRequest.getPrincipalId());
 		event.setCreatedAt(LocalDateTime.now().format(
-				DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.ss'Z'")));
+				DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")));
 		event.setBody(apiRequest.getContent());
 
 		return event;
